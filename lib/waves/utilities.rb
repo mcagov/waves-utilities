053f3988c867f9e3eda4cc5a5479a01bbe88d918
Toby Privett <toby.privett@oceanshq.com>
2016-09-12 14:32:07 +0100

    Add gem files

---
+++ lib/waves/utilities.rb
@@ -0,0 +1,7 @@
+require "waves/utilities/version"
+
+module Waves
+  module Utilities
+    # Your code goes here...
+  end
+end
---
+++ lib/waves/utilities/version.rb
@@ -0,0 +1,5 @@
+module Waves
+  module Utilities
+    VERSION = "0.1.0"
+  end
+end
