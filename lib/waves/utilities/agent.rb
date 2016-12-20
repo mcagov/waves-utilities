29c97bc4b4551a19cdf81b7c24c3ca5c26589a83
Toby Privett <toby@snaplab.co.uk>
2016-12-20 15:38:47 +0000

    Build an attributes hash for the agent class

--- lib/waves/utilities/agent.rb
+++ lib/waves/utilities/agent.rb
@@ -1,4 +1,17 @@
 module WavesUtilities
   class Agent < WavesUtilities::Customer
+    def attributes
+      {
+        name: @name,
+        email: @email,
+        phone_number: @phone_number,
+        address_1: @address_1,
+        address_2: @address_2,
+        address_3: @address_3,
+        town: @town,
+        postcode: @postcode,
+        country: @country
+      }
+    end
   end
 end
--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -2,4 +2,5 @@ require "spec_helper"
 
 describe WavesUtilities::Agent do
   it { expect(described_class).to be < WavesUtilities::Customer }
+  it { expect(described_class).respond_to?(:attributes) }
 end
