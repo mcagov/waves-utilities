1783e0bca06eaed790108dc9c5b80431180066c0
Toby Privett <toby@snaplab.co.uk>
2017-02-16 13:56:11 +0000

    Stub out Fee Schedule for all parts
    - and remove :task until we need it

--- lib/waves/utilities/fee_schedule.rb
+++ lib/waves/utilities/fee_schedule.rb
@@ -1,11 +1,18 @@
 class WavesUtilities::FeeSchedule
+  PREMIUM_SURPLUS = 5000
+
   class << self
-    def standard_fee(part = :part_3, _task = :new_registration)
-      2500 if part == :part_3
+    def standard_fee(part = :part_3)
+      case part.to_sym
+      when :part_1 then 2500
+      when :part_2 then 2500
+      when :part_3 then 2500
+      when :part_4 then 2500
+      end
     end
 
-    def premium_fee(part = :part_3, task = :new_registration)
-      standard_fee(part, task) + 5000
+    def premium_fee(part = :part_3)
+      standard_fee(part) + PREMIUM_SURPLUS
     end
   end
 end
--- spec/fee_schedule_spec.rb
+++ spec/fee_schedule_spec.rb
@@ -2,10 +2,26 @@ require "spec_helper"
 
 describe WavesUtilities::FeeSchedule do
   context "#standard_fee" do
-    it { expect(described_class.standard_fee).to eq(2500) }
+    it "retrieves a fee for each part" do
+      [:part_1, :part_2, :part_3, :part_4].each do |part|
+
+        expect(WavesUtilities::FeeSchedule.standard_fee(part))
+          .to be_a(Integer)
+      end
+    end
   end
 
   context "#premium_fee" do
-    it { expect(described_class.premium_fee).to eq(7500) }
+    it "retrieves a premium_fee for each part" do
+      [:part_1, :part_2, :part_3, :part_4].each do |part|
+
+        premium_fee =
+          WavesUtilities::FeeSchedule.standard_fee(part) +
+            WavesUtilities::FeeSchedule::PREMIUM_SURPLUS
+
+        expect(WavesUtilities::FeeSchedule.premium_fee(part))
+          .to eq(premium_fee)
+      end
+    end
   end
 end
