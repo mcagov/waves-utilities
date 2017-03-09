764fe2afd4d440077d20ce54d1399fa3f14440f9
Toby Privett <toby@snaplab.co.uk>
2017-03-09 10:34:04 +0000

    Add registration types for all parts

--- lib/waves/utilities/registration_type.rb
+++ lib/waves/utilities/registration_type.rb
@@ -1,11 +1,16 @@
 module WavesUtilities
   class RegistrationType
-    def self.all
-      [
-        :simple,
-        :full,
-        :provisional,
-      ].freeze
+    class << self
+      def in_part(part)
+       case part.to_sym
+        when :part_1
+          [:commercial, :pleasure]
+        when :part_2
+          [:simple, :full, :provisional]
+        when :part_4
+          [:fishing, :commercial, :pleasure]
+        end
+      end
     end
   end
 end
--- spec/registration_type_spec.rb
+++ spec/registration_type_spec.rb
@@ -1,11 +1,22 @@
 require "spec_helper"
 
 RSpec.describe WavesUtilities::RegistrationType do
-  context ".all" do
-    subject { described_class.all }
+  context ".in_part" do
+    subject { described_class.in_part(part) }
 
-    it "lists the vessel_types" do
-      expect(subject.first).to eq(:simple)
+    context "part_1" do
+      let(:part) { "part_1" }
+      it { expect(subject.first).to eq(:commercial) }
+    end
+
+    context "part_2" do
+      let(:part) { :part_2 }
+      it { expect(subject.first).to eq(:simple) }
+    end
+
+    context "part_4" do
+      let(:part) { :part_4 }
+      it { expect(subject.first).to eq(:fishing) }
     end
   end
 end
