4444b811d5275e0f00b8240a2f8c3e327365339e
Toby Privett <toby@snaplab.co.uk>
2017-05-02 16:47:21 +0100

    Enable Closure model

---
+++ lib/waves/utilities/closure.rb
@@ -0,0 +1,23 @@
+module WavesUtilities
+  class Closure
+    REASONS =
+      [
+        :sold,
+        :destroyed,
+        :registered_elsewhere,
+      ].freeze
+
+    attr_accessor(
+      :reason,
+      :current_step,
+      :actioned_day,
+      :actioned_month,
+      :actioned_year,
+      :new_owner_name,
+      :new_owner_email,
+      :new_owner_phone_number,
+      :destruction_method,
+      :new_flag,
+      :new_flag_reason)
+  end
+end
