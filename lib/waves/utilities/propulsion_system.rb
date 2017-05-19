16c10dd3ed8846af387e2edba4b1b0ab166546ae
Toby Privett <toby@snaplab.co.uk>
2017-05-19 10:29:34 +0100

    Update and simplify Propulsion Systems

--- lib/waves/utilities/propulsion_system.rb
+++ lib/waves/utilities/propulsion_system.rb
@@ -1,27 +1,8 @@
 class WavesUtilities::PropulsionSystem
-  def initialize(code)
-    @code = code.to_sym
-  end
-
-  def name
-    ret =
-      WavesUtilities::PropulsionSystem.all.find do
-        |propulsion_system| propulsion_system[1] == @code
-    end
-    ret ? ret[0] : ""
-  end
-
   class << self
     def all
       [
-        ["None (dumb)", :none],
-        ["Sail (auxiliary motor only)", :sail],
-        ["Sail and motor", :sail_and_motor],
-        ["Steam", :steam],
-        ["Outboard diesel", :outboard_diesel],
-        ["Outboard petrol", :outboard_petrol],
-        ["Internal diesel", :inboard_diesel],
-        ["Internal petrol", :inboard_petrol]
+        "Sail", "Motor", "Sail & Motor", "Steam", "None (dumb)"
       ]
     end
   end
--- spec/propulsion_system_spec.rb
+++ spec/propulsion_system_spec.rb
@@ -1,27 +1,9 @@
 require "spec_helper"
 
 RSpec.describe WavesUtilities::PropulsionSystem do
-  context "#name" do
-    let(:propulstion_system) { described_class.new(code) }
-
-    subject { propulstion_system.name }
-
-    context "with a valid code" do
-      let(:code) { "steam" }
-
-      it { expect(subject).to eq("Steam") }
-    end
-
-    context "with an invalid port_code" do
-      let(:code) { "FOOOO" }
-
-      it { expect(subject).to eq("") }
-    end
-  end
-
   context ".all" do
     subject { described_class.all }
 
-    it { expect(subject.first).to eq(["None (dumb)", :none]) }
+    it { expect(subject.first).to eq("Sail") }
   end
 end
