324bea2a929c0763609cb20143449683ada82f2a
Toby Privett <toby@snaplab.co.uk>
2017-05-22 16:43:22 +0100

    Update Document Types

--- lib/waves/utilities/document_type.rb
+++ lib/waves/utilities/document_type.rb
@@ -23,10 +23,17 @@ class WavesUtilities::DocumentType
         ["Carving & Marking Note", "carving_and_marking"],
         ["Certificate of Survey", "certificate_of_survey"],
         ["Engine Manufacturers Data Sheet(s)", "engine_manufacturers_data_sheet"],
+        ["Engine Receipt", "engine_recipient"],
         ["Fishing Vessel Safety Certificate", "fishing_vessel_safety_certificate"],
         ["International Tonnage Certificate", "international_tonnage_certificate"],
+        ["MCEP Form", "mcep_form"],
         ["Measurement Certificate", "measurement_certificate"],
         ["Mortgage Deed", "mortgage_deed"],
+        ["Other Sale Document", "other_sale_document"],
+        ["Seafish Construction Certificate", "seafish_construction_certificate"],
+        ["Seafish Inspection Report", "seafish_inspection_report"],
+        ["Seafish email from Principal Surveyor", "seafish_email_from_principal_surveyor"],
+        ["Written Correspondence", "written_correspondence"],
       ]
     end
   end
