3218759238f3a06e747c71b055770889e1f9a0b9
Toby Privett <toby@snaplab.co.uk>
2017-06-01 12:31:28 +0100

    Add eligibility status for part 4 non-fishing

--- lib/waves/utilities/eligibility_status.rb
+++ lib/waves/utilities/eligibility_status.rb
@@ -8,5 +8,18 @@ class WavesUtilities::EligibilityStatus
         ["(d) A local authority in the United Kingdom.", :status_d],
       ]
     end
+
+    def part_4_non_fishing
+      [
+        ["(a) British citizens or non-United Kingdom nationals exercising their right of freedom of movement of workers or right of establishment under Article 48 or 52 of the EEC Treaty or Article 28 or 31 of the EEA Agreement", :status_a],
+        ["(b) British Dependent Territories citizens", :status_b],
+        ["(c) British Overseas citizens", :status_c],
+        ["(d) Persons who under the British Nationality Order 1981 are British subjects", :status_d],
+        ["(e) Persons who under the Hong Kong (British Nationality) Order 1986 are British Nationals (Overseas)", :status_e],
+        ["(f) Bodies corporate incorporated in an EEA state", :status_f],
+        ["(g) Bodies corporate incorporated in any relevant British Possession and having their principal place of business in the UK or in any such possession", :status_g],
+        ["(h) European Economic Interest Groupings being groupings formed in pursuance of Article 1 of Council Regulation (EEC) No. 2137/85 and registered in the UK", :status_h],
+      ]
+    end
   end
 end
