c8300d987294245c9dc811c7494612745860b602
Toby Privett <toby@snaplab.co.uk>
2017-06-02 12:00:37 +0100

    Simplify the FeeSchedule
    - We are currently only using this in govuk for part_3, so let's remove
    any reference to the other parts until the pricing system is propery
    implemented in waves. Then we can look at restoring it the gem.

--- lib/waves/utilities/fee_schedule.rb
+++ lib/waves/utilities/fee_schedule.rb
@@ -2,17 +2,12 @@ class WavesUtilities::FeeSchedule
   PREMIUM_SURPLUS = 5000
 
   class << self
-    def standard_fee(part = :part_3)
-      case part.to_sym
-      when :part_1 then 2500
-      when :part_2 then 2500
-      when :part_3 then 2500
-      when :part_4 then 2500
-      end
+    def standard_fee
+      2500
     end
 
-    def premium_fee(part = :part_3)
-      standard_fee(part) + PREMIUM_SURPLUS
+    def premium_fee
+      standard_fee + PREMIUM_SURPLUS
     end
   end
 end
--- spec/fee_schedule_spec.rb
+++ spec/fee_schedule_spec.rb
@@ -2,26 +2,14 @@ require "spec_helper"
 
 describe WavesUtilities::FeeSchedule do
   context "#standard_fee" do
-    it "retrieves a fee for each part" do
-      [:part_1, :part_2, :part_3, :part_4].each do |part|
-
-        expect(WavesUtilities::FeeSchedule.standard_fee(part))
-          .to be_a(Integer)
-      end
+    it "retrieves a fee" do
+      expect(WavesUtilities::FeeSchedule.standard_fee).to eq(2500)
     end
   end
 
   context "#premium_fee" do
-    it "retrieves a premium_fee for each part" do
-      [:part_1, :part_2, :part_3, :part_4].each do |part|
-
-        premium_fee =
-          WavesUtilities::FeeSchedule.standard_fee(part) +
-            WavesUtilities::FeeSchedule::PREMIUM_SURPLUS
-
-        expect(WavesUtilities::FeeSchedule.premium_fee(part))
-          .to eq(premium_fee)
-      end
+    it "retrieves a fee" do
+      expect(WavesUtilities::FeeSchedule.premium_fee).to eq(7500)
     end
   end
 end
