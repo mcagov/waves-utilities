3abda7b3895f4eb64799719501968e3a18cc61f2
Toby Privett <toby@snaplab.co.uk>
2017-08-25 15:37:01 +0100

    Build the nationality list from the ISO3166::Country gem

--- lib/waves/utilities/nationality.rb
+++ lib/waves/utilities/nationality.rb
@@ -2,70 +2,10 @@ module WavesUtilities
   class Nationality
     class << self
       def all
-        raw_list.map(&:first)
-      end
+        nationalities =
+          (ISO3166::Country.all.map(&:nationality) - ["", nil]).map(&:upcase)
 
-      private
-
-      def raw_list
-        [
-          ['UNITED KINGDOM','British Citizen'],
-          ['AKROTIRI AND DHEKELIA','British Overseas Territory (British Dependent Territory)'],
-          ['ANGUILLA','British Overseas Territory (British Dependent Territory)'],
-          ['ANTIGUA BRD','Commonwealth Citizen'],
-          ['AUSTRALIA','Commonwealth Citizen'],
-          ['AUSTRIA','European Union Country'],
-          ['BAHAMAS','Commonwealth Citizen'],
-          ['BELGIUM','European Union Country'],
-          ['BERMUDA','British Overseas Territory (British Dependent Territory)'],
-          ['BRITISH VIRGIN ISLANDS','British Overseas Territory (British Dependent Territory)'],
-          ['BULGARIA','European Union Country'],
-          ['CANADA','Commonwealth Citizen'],
-          ['CAYMAN ISLANDS','British Overseas Territory (British Dependent Territory)'],
-          ['CHANNEL ISLANDS','British Citizen'],
-          ['CROATIA','European Union Country'],
-          ['CYPRUS','European Union Country'],
-          ['CZECH REPUBLIC','European Union Country'],
-          ['DENMARK','European Union Country'],
-          ['DOMINICA','Commonwealth Citizen'],
-          ['EIRE','European Union Country'],
-          ['ESTONIA','European Union Country'],
-          ['FINLAND','European Union Country'],
-          ['FRANCE','European Union Country'],
-          ['GERMANY','European Union Country'],
-          ['GIBRALTAR','British Overseas Territory (British Dependent Territory)'],
-          ['GREECE','European Union Country'],
-          ['HUNGARY','European Union Country'],
-          ['ICELAND','European Economic Area Country'],
-          ['INDIA','Commonwealth Citizen'],
-          ['ISLE OF MAN','British Citizen'],
-          ['ITALY','European Union Country'],
-          ['KENYA','Commonwealth Citizen'],
-          ['LATVIA','European Union Country'],
-          ['LIECHTENSTEIN','European Economic Area Country'],
-          ['LITHUANIA','European Union Country'],
-          ['LUXEMBOURG','European Union Country'],
-          ['MALAYSIA','Commonwealth Citizen'],
-          ['MALTA','European Union Country'],
-          ['NAURU','Commonwealth Citizen'],
-          ['NETHERLANDS','European Union Country'],
-          ['NEW ZEALAND','Commonwealth Citizen'],
-          ['NIGERIA','Commonwealth Citizen'],
-          ['NORWAY','European Economic Area Country'],
-          ['POLAND','European Union Country'],
-          ['PORTUGAL','European Union Country'],
-          ['ROMANIA','European Union Country'],
-          ['SINGAPORE','Commonwealth Citizen'],
-          ['SLOVAKIA','European Union Country'],
-          ['SLOVENIA','European Union Country'],
-          ['SOUTH AFRICA','Commonwealth Citizen'],
-          ['SPAIN','European Union Country'],
-          ['SWEDEN','European Union Country'],
-          ['SWITZERLAND','European Economic Area Country'],
-          ['TANZANIA','Commonwealth Citizen'],
-          ['TRINIDAD & TOBAGO','Commonwealth Citizen'],
-          ['TURKEY','European Economic Area Country'],
-        ]
+        (nationalities -= ["BRITISH"]).insert(0, "BRITISH")
       end
     end
   end
--- spec/nationality_spec.rb
+++ spec/nationality_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe WavesUtilities::Nationality do
     subject { described_class.all }
 
     it "lists the nationalities" do
-      expect(subject.first).to eq("UNITED KINGDOM")
+      expect(subject.first).to eq("BRITISH")
+    end
+
+    it "contains BRITISH once" do
+      expect(subject.count("BRITISH")).to eq(1)
     end
   end
 end
