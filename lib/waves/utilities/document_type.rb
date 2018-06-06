c2ee9ee94274df932507607a1f2854fff1d91ba8
Toby Privett <toby@snaplab.co.uk>
2018-06-06 13:14:49 +0100

    Prefer title case document types

--- lib/waves/utilities/document_type.rb
+++ lib/waves/utilities/document_type.rb
@@ -19,14 +19,14 @@ class WavesUtilities::DocumentType
         ["Certificate of Incorporation", :certificate_of_incorporation],
         ["Certificate of Survey for Tonnage & measurement", :certificate_of_survey],
         ["Charter Party Agreement", :charter_party_agreement],
-        ["Coding certificate", :certificate],
+        ["Coding Certificate", :certificate],
         ["Consent Letter", :consent_letter],
         ["Continuous Synopsis Record", :continous_synopsis_record],
         ["Current Certificate of Registry", :current_certificate_of_registry],
         ["Death Certificate", :death_certificate],
         ["Declaration of Eligibility", :declaration_of_elgibility],
         ["Deletion Certificate or extract", :deletion_certificate],
-        ["Engine Spec sheet", :engine_spec_sheet],
+        ["Engine Spec Sheet", :engine_spec_sheet],
         ["Invoice", :invoice],
         ["ITC (if vessel is 24m or more)", :itc],
         ["Marriage Certificate", :marriage_certificate],
@@ -34,7 +34,7 @@ class WavesUtilities::DocumentType
         ["Safety Certificate", :safety_certificate],
         ["Seafish", :seafish],
         ["Outside UK Water letter", :ouside_uk_water_letter],
-        ["Written correspondence from the owner(s)", :written_correspondence]
+        ["Written Correspondence from the owner(s)", :written_correspondence]
       ]
     end
   end
